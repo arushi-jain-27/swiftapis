cb488ac4e9116b3f802d7ce629c7f1a1683e2d50
Arushi Jain <arushijain1154@gmail.com>
2018-06-19 01:43:28 +0530

    Hyperlinked APIs and removed comments from views.py

--- files/views.py
+++ files/views.py
@@ -2,59 +2,8 @@
 from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 import requests, os
+from rest_framework.reverse import reverse
 
-"""
-def get_acc (request):
-    form = AccountForm(request.POST or None)
-    if form.is_valid():
-        obj = form.save(commit=False)
-        token = obj.token
-        url= 'http://10.129.103.86:8080/v1/AUTH_b3f70be8acad4ec197e2b5edf48d9e5a/'
-        r = requests.get(url, headers={'X-Auth-Token': token}).text
-        obj_arr = r.split("\n")
-        obj_arr.pop()
-        return render (request, 'files/display_cont.html', {"obj_arr": obj_arr, "token":token})
-    context = {
-        "form": form,
-    }
-    return render(request, 'files/input.html', context)
-	
-def search_object (request):
-    form = FilesForm(request.POST or None)
-    if form.is_valid():
-        obj = form.save(commit=False)
-        container = obj.container
-        object_name = obj.object
-        token = obj.token
-        location = obj.location
-        url= 'http://10.129.103.86:8080/v1/AUTH_b3f70be8acad4ec197e2b5edf48d9e5a/' + container + '/'+ object_name
-        r = requests.get(url, headers={'X-Auth-Token': token})
-        #return r
-        with open(location, "wb") as code:
-         code.write(r.content)
-        return render (request, 'files/success.html')
-    context = {
-        "form": form,
-    }
-    return render(request, 'files/input.html', context)
-
-def get_cont (request, container, token):
-     url= 'http://10.129.103.86:8080/v1/AUTH_b3f70be8acad4ec197e2b5edf48d9e5a/' + container
-     r = requests.get(url, headers={'X-Auth-Token': token}).text
-     #r = r.decode ("utf-8")
-     obj_arr = r.split("\n")
-     obj_arr.pop()
-     return render (request, 'files/display_objects.html', {"obj_arr": obj_arr, "container":container, "token":token})
-
-
-def get_obj(request, container,object, token):
-    url = 'http://10.129.103.86:8080/v1/AUTH_b3f70be8acad4ec197e2b5edf48d9e5a/' + container + '/'+ object
-    r = requests.get(url, headers={'X-Auth-Token': token})
-    with open(object, "wb") as code:
-      code.write(r.content)
-    return render (request, 'files/success.html')
-
-"""
 
 @api_view(['GET', 'DELETE'])
 def download_object(request, container, object, format=None):
@@ -84,7 +33,13 @@ def container_list(request, format=None):
         r = requests.get('http://10.129.103.86:8080/v1/AUTH_b3f70be8acad4ec197e2b5edf48d9e5a/', headers={'X-Auth-Token': token}).text
         obj_arr = r.split ("\n")
         obj_arr.pop()
-        return Response (obj_arr)
+        rows = len(obj_arr)
+        columns = 2
+        Matrix = [[0 for x in range(columns)] for x in range(rows)]
+        for i in range(rows):
+            Matrix[i][0] = obj_arr[i]
+            Matrix[i][1] = reverse('files:cont_info', kwargs={'container': obj_arr[i]}, request=request, format=format)
+        return Response(Matrix)
     if request.method =='PUT':
         new_cont = request.data
         r = requests.put ('http://10.129.103.86:8080/v1/AUTH_b3f70be8acad4ec197e2b5edf48d9e5a/' + new_cont, headers={'X-Auth-Token': token}).text
@@ -102,7 +57,14 @@ def object_list(request, container, format=None):
         r = requests.get('http://10.129.103.86:8080/v1/AUTH_b3f70be8acad4ec197e2b5edf48d9e5a/'+container , headers={'X-Auth-Token': token}).text
         obj_arr = r.split ("\n")
         obj_arr.pop()
-        return Response (obj_arr)
+        rows = len(obj_arr)
+        columns = 2
+        Matrix = [[0 for x in range(columns)] for x in range(rows)]
+        for i in range(rows):
+            Matrix[i][0] = obj_arr[i]
+            Matrix[i][1] = reverse('files:obj_info', kwargs={'container': container, 'object': obj_arr[i]},
+                                   request=request, format=format)
+        return Response(Matrix)
     if request.method =='PUT':
         url = request.data
         obj = os.path.basename(url)
